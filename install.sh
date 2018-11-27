94f72c98787582338439e798ec1b6965a764cfc2
UndiAbler <undiabler@gmail.com>
2018-11-27 18:12:52 +0100

    noconfirm

--- install.sh
+++ install.sh
@@ -8,10 +8,10 @@ LOCALREPO_VC_DIR=$LOCALREPO/.git
 
 
 echo "Install i3wm..."
-sudo pacman -S --needed i3-gaps i3lock i3blocks git python-jinja python-requests rxvt-unicode scrot compton
+sudo pacman -S --needed --noconfirm i3-gaps i3lock i3blocks git python-jinja python-requests rxvt-unicode scrot compton
 
 echo "Install fonts..."
-sudo pacman -S --needed ttf-ubuntu-font-family terminus-font
+sudo pacman -S --needed --noconfirm ttf-ubuntu-font-family terminus-font
 #yaourt --noconfirm --needed awesome-terminal-fonts-git
 
 echo "Install i3 autoconfig..."
